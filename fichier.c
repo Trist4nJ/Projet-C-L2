021ecea8ab96237a4f6edc348fc8e24c8fd386a8
Tristan <martineztristan317@gmail.com>
2023-10-31 14:43:58 +0100

    test benoi

--- fichier.c
+++ fichier.c
@@ -22,47 +22,41 @@ p_cell createcell(int val, int level)
     return newcell;
 }
 
-t_d_list emptylist(int level)
+t_d_list emptylist(int hauteur)
 {
     t_d_list list;
     list.head = (t_d_cell**) malloc(sizeof(t_d_cell));
-    list.max_level = level;
-    for (int i = 0; i<level; i++)
+    list.max_level = hauteur;
+    for (int i = 0; i<hauteur; i++)
     {
         list.head[i] = NULL;
     }
     return list;
 }
 
-void addheadcell(t_d_list * list, p_cell cell)
+void addheadcell(t_d_list* list, p_cell cell)
 {
-    for (int i = 0; i<cell->level;i++)
+    if(list->head[level] == NULL)
+        list->head[level] = cell;
+    else
     {
-        if (list->head[i] == NULL)
-        {
-            list->head[i] = cell;
-        }
-        else
-        {
-            cell->next[i] = list->head[i];
-            list->head[i] = cell;
-        }
+        p_cell tmp;
+        tmp = list->head[level];
+        list->head[level] = cell;
+        cell->next = tmp;
     }
 }
-
 
 void displaylist (t_d_list* list, int level)
 {
     if (list->head[level] == NULL)
     {
-        printf("[ list  head_%d @-] --> ", level);
-
+        printf("[ list  head_%d @-] --> ", level );
         printf("NULL ");
     }
     else
     {
         printf("[ list  head_%d @-] --> ", level);
-
         for (p_cell cell = list->head[level]; cell != NULL; cell = cell->next[level]) {
             printf("[ %d | @- ] --> ",cell->value);
         }
@@ -75,11 +69,10 @@ void displaylistlevel(t_d_list *list)
     for (int i = 0; i < list->max_level; i++)
     {
         displaylist(list, i);
-        printf("\n");
     }
 }
 
-void insertcelllist(t_d_list* list, int level, p_cell newcell)
+void insertCellList(t_d_list* list, int level, p_cell newcell)
 {
     if(list->head[level] == NULL)
     {
@@ -93,10 +86,10 @@ void insertcelllist(t_d_list* list, int level, p_cell newcell)
     while(tmp != NULL && newcell->value > tmp->value)
     {
         ptmp = tmp;
-        tmp = tmp->next;
+        tmp = *tmp->next;
     }
-    ptmp->next = newcell;
-    newcell->next = tmp;
+    ptmp->next = &newcell;
+    newcell->next = &tmp;
     return;
 }
 
